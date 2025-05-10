0ac856a5e0dfdffe0cc8032cbaa88d8b091a8084
Shrishabayari <bayarishrisha@gmail.com>
2025-05-10 16:40:07 +0530

    update

--- ev-charge-hub/client/src/components/admin/AdminDashboard.jsx
+++ ev-charge-hub/client/src/components/admin/AdminDashboard.jsx
@@ -42,7 +42,6 @@ const AdminDashboard = () => {
           View Booking Slots
         </button>
         <button onClick={handleViewMyBookings} className="bg-yellow-500 text-white p-3 rounded">
-          View My Bookings
         </button>
         <button onClick={handleViewAdminBookings} className="bg-red-500 text-white p-3 rounded">
           View Admin Bookings
